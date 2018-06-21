f1a6279bf64231aa726c3a413290312586ece321
alexis_peloso <alexis.peloso@gmail.com>
2018-06-21 18:58:28 -0300

    creo metodo mano_comp

--- juego_ejer.py
+++ juego_ejer.py
@@ -1,6 +1,16 @@
 # Juego de piedra, papel y tijera
 
+# 1=piedra
+# 2=pàpel
+# 3=tijera
+
 class Juego():
+	manocpu=0
 	def __init__(self,mano):
 		self.mano=mano
+
+	def Mano_Comp(self):
+		from random import randint
+		self.manocpu=randint(1, 3)
+		return self.manocpu
 	
